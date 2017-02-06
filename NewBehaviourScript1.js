afc9be67cd593bcab905573932ddc47d2f5ce25a
angel-marchev <angel.marchev@yahoo.com>
2017-02-06 19:14:54 +0200

    Add files via upload

---
+++ NewBehaviourScript1.js
@@ -0,0 +1,7 @@
+﻿#pragma strict
+
+function FixedUpdate () {
+ 
+    transform.localPosition.y += 6 * Time.deltaTime;
+
+}
---
+++ Scroll.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scroll : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y + 6 * Time.deltaTime, transform.localPosition.z);
+    }
+}
